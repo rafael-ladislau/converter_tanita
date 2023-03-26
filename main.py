a13bd713afd6416123c5771aa1d7d922b50bfdde
Rafael Ladislau <ladis@darwinseguros.com>
2023-03-26 15:24:12 -0300

    First release ready version

--- main.py
+++ main.py
@@ -1,42 +1,119 @@
 import tkinter as tk
-import tkinter.filedialog as filedialog
+from tkinter import filedialog, messagebox
+import re
+import csv
+import datetime
 
-class TextEditor:
-    def __init__(self, master):
-        self.master = master
-        master.title("Criação do arquivo CSV Tanita")
+headers = {
+    'Weight kg': '"Weight (kg)"',
+    'BMI BMI': 'BMI',
+    'Body fat %': '"Body Fat (%)"',
+    'Torso %': '"Body fat (%) - trunk"',
+    'Left arm %': '"Body fat (%) - left arm"',
+    'Right arm %': '"Body fat (%) - right arm"',
+    'Left leg %': '"Body fat (%) - left leg"',
+    'Right leg %': '"Body fat (%) - right leg"',
+    'Muscle mass kg': '"Muscle Mass (kg)"',
+    'Torso kg': '"Muscle mass - trunk"',
+    'Left arm kg': '"Muscle mass - left arm"',
+    'Right arm kg': '"Muscle mass - right arm"',
+    'Left leg kg': '"Muscle mass - left leg"',
+    'Right leg kg': '"Muscle mass - right leg"',
+    'Muscle quality MQ': '"Muscle Quality"',
+    'Left arm MQ': '"Muscle quality - left arm"',
+    'Right arm MQ': '"Muscle quality - right arm"',
+    'Left leg MQ': '"Muscle quality - left leg"',
+    'Right leg MQ': '"Muscle quality - right leg"',
+    'Body type': '"Physique Rating"',
+    'Bone mass kg': '"Bone Mass (kg)"',
+    'Visceral fat': '"Visc Fat"',
+    'BMR kcal': '"BMR (kcal)"',
+    'Metabolic age years': '"Metab Age"',
+    'Body water %': '"Body Water (%)"'
+}
 
-        # Create label with 300-character text
-        label_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis metus ut ex laoreet malesuada. Morbi tempor fermentum urna eu consectetur. Sed et dolor blandit, vulputate augue ac, malesuada odio. Fusce non nisl vitae ipsum malesuada dapibus. Nullam non maximus purus, in commodo nulla. Vivamus euismod placerat faucibus. Integer sit amet justo blandit, consequat arcu non, vulputate elit. Duis ultrices hendrerit mauris sit amet fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec tristique mi at tellus vestibulum, at dictum dui pretium. Suspendisse a commodo ipsum. Nulla id ultrices tortor. Maecenas ac est justo."
-        self.label = tk.Label(master, text=label_text, wraplength=580)
+sorted_headers = ['Date','"Weight (kg)"','BMI','"Body Fat (%)"','"Visc Fat"','"Muscle Mass (kg)"','"Muscle Quality"','"Bone Mass (kg)"','"BMR (kcal)"','"Metab Age"','"Body Water (%)"','"Physique Rating"','"Muscle mass - right arm"','"Muscle mass - left arm"','"Muscle mass - right leg"','"Muscle mass - left leg"','"Muscle mass - trunk"','"Muscle quality - right arm"','"Muscle quality - left arm"','"Muscle quality - right leg"','"Muscle quality - left leg"','"Muscle quality - trunk"','"Body fat (%) - right arm"','"Body fat (%) - left arm"','"Body fat (%) - right leg"','"Body fat (%) - left leg"','"Body fat (%) - trunk"','"Heart rate"']
+def submit_text():
+    text = text_area.get("1.0", "end-1c")  # get text from text area
 
-        self.label.pack(fill="both", expand=True)
+    # Define a regular expression pattern to capture numbers after colon
+    pattern = r'[^a-zA-Z]*(.+):\s*([\d,\.]+)\s*([%a-zA-z]*)'
 
-        # Create text area
-        self.text_area = tk.Text(master)
-        self.text_area.pack()
+    # Find all matches of the pattern in the text and store them in a dictionary
+    matches = re.findall(pattern, text)
+    if not matches:
+        messagebox.showwarning("Texto inválido", "O texto colado não foi reconhecido como um resultado WhatsApp ou email da Tanita.")
+    else:
+        results = {}
+        for match in matches:
+            group_name = match[0]
+            if match[2] != '':
+                group_name = group_name + ' ' + match[2]
+            value = float(match[1].replace('.', '').replace(',', '.'))
+            results[headers[group_name]] = value
 
-        # Create submit button
-        self.submit_button = tk.Button(master, text="Submit", command=self.submit_text)
-        self.submit_button.pack()
+        results['Date'] = '"'+datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+'"'
+        results['"Muscle quality - trunk"'] = '-'
+        results['"Heart rate"'] = '-'
 
-    def submit_text(self):
-        # Get text from text area and capitalize it
-        text = self.text_area.get("1.0", "end-1c")
-        capitalized_text = text.upper()
+        final_result = {}
 
-        # Ask user for file name and location
-        file_path = filedialog.asksaveasfilename(defaultextension=".txt")
+        for header in sorted_headers:
+            final_result[header] = results[header]
 
-        # Save capitalized text to file
-        with open(file_path, "w") as file:
-            file.write(capitalized_text)
+        filename = filedialog.asksaveasfilename(defaultextension=".csv", initialfile="resultado.csv")  # get filename from user
+        if filename:
+            with open(filename, 'w', newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=final_result.keys(), quoting=csv.QUOTE_NONE, quotechar="'")
+                writer.writeheader()
+                writer.writerow(final_result)
+        messagebox.showinfo('Sucesso', 'O arquivo foi salvo com sucesso!')
 
-# Create Tkinter window
 root = tk.Tk()
-root.geometry("600x500")
-# Create TextEditor object
-text_editor = TextEditor(root)
 
-# Run the Tkinter event loop
+# get screen dimensions
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# calculate window position
+window_width = 600
+window_height = 500
+x_pos = (screen_width - window_width) // 2
+y_pos = (screen_height - window_height) // 2
+
+# set window geometry and title
+root.geometry(f"{window_width}x{window_height}+{x_pos}+{y_pos}")
+root.resizable(False, False)
+root.configure(padx=10, pady=10)
+root.title("Criação do arquivo CSV Tanita")
+
+# Create a label with wrapped text
+label_text = "Cole abaixo o resultado recebido por e-mail ou WhatsApp. Clique no botão \"Enviar\" para salvar o arquivo."
+label = tk.Label(root, text=label_text, wraplength=580)
+label.pack(fill="both", expand=True, pady=20)
+
+# Create a frame to hold the text area and submit button
+frame = tk.Frame(root)
+
+# Create a label with 300 characters of text
+# intro_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget ante faucibus, congue lorem vitae, pulvinar nibh. Etiam sed tellus tellus. Nullam commodo, massa eget dignissim mollis, ipsum nunc vestibulum mauris, ut commodo tortor velit vel elit. Donec tincidunt justo sit amet commodo fringilla. Duis fringilla nisl nec mi molestie commodo. Suspendisse tincidunt euismod neque, at scelerisque enim bibendum ac. Proin vel risus justo. Integer sit amet dolor non velit luctus molestie."
+
+# intro_label = tk.Label(frame, text=intro_text, wraplength=580, justify="left")
+# intro_label.pack(side="top")
+
+# Create a text area and submit button
+text_area = tk.Text(frame)
+submit_button = tk.Button(frame, text="Enviar", command=submit_text)
+
+# Pack the text area and submit button
+text_area.pack(fill="both", expand=True)
+submit_button.pack(side="bottom")
+
+# Add an empty line below the submit button
+tk.Label(frame, text="").pack()
+
+# Pack the frame to fill the window
+frame.pack(fill="both", expand=True)
+
 root.mainloop()
+
